1c0e198a1d31324e6edd6091f7e9847af21f61a5
Sam Sneed <163201376+sam-sneed@users.noreply.github.com>
2024-06-05 01:42:51 +0000

    Update sneedscan.sh

--- sneedscan.sh
+++ sneedscan.sh
@@ -1,5 +1,7 @@
 echo SneedScan - a Sneed Group Tool.
 sudo freshclam
+
+echo Scanning home folders....
 sudo clamscan -r -i --scan-pe=yes \
 --scan-elf=yes \
 --scan-ole2=yes \
@@ -12,5 +14,36 @@ sudo clamscan -r -i --scan-pe=yes \
 --alert-macros=yes \
 --max-files=10000 \
 --max-recursion=12 \
---max-dir-recursion=420 \
---remove /
+--max-dir-recursion=10 \
+--remove /home
+
+
+echo Scanning user binaries....
+--scan-elf=yes \
+--scan-ole2=yes \
+--scan-pdf=yes \
+--scan-xmldocs=yes \
+--scan-archive=no \
+--scan-swf=no \
+--scan-html=no \
+--scan-hwp3=no \
+--alert-macros=yes \
+--max-files=10000 \
+--max-recursion=12 \
+--max-dir-recursion=3 \
+--remove /usr/bin
+
+echo Scanning system binaries.
+--scan-elf=yes \
+--scan-ole2=yes \
+--scan-pdf=yes \
+--scan-xmldocs=yes \
+--scan-archive=no \
+--scan-swf=no \
+--scan-html=no \
+--scan-hwp3=no \
+--alert-macros=yes \
+--max-files=10000 \
+--max-recursion=12 \
+--max-dir-recursion=3 \
+--remove /bin
